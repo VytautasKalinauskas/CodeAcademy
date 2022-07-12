b28658b9ca590a045da1383c6b5791b5abf6510f
Vytautas Kalinauskas <vytautas.kalinauskas@rivile.lt>
2022-07-12 19:38:39 +0300

    istrinu failus

--- test paskaita - github/SvarbiKlaida.java
+++
@@ -1,9 +0,0 @@
-package com.company;
-
-public class SvarbiKlaida extends VaziuoklesIsskleidimoKlaida {
-
-    public SvarbiKlaida(String message, String priezastisParam) {
-        super(message, priezastisParam);
-    }
-
-}
