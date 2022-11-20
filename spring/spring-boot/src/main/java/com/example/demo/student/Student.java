bfeab57c86e935b82157e6d1e21c560d0ef5d630
Rokas Paulauskas <rokas_nkt@yahoo.com>
2022-11-20 11:04:07 +0200

    Aplikacija prijungta prie ProstgreSQL db. Student esybė, dto, kontroleris, repo, mapperis

---
+++ spring/spring-boot/src/main/java/com/example/demo/student/Student.java
@@ -0,0 +1,53 @@
+package com.example.demo.student;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "students")
+public class Student {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "personal_code")
+    private String personalCode;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPersonalCode() {
+        return personalCode;
+    }
+
+    public void setPersonalCode(String personalCode) {
+        this.personalCode = personalCode;
+    }
+}
---
+++ spring/spring-boot/src/main/java/com/example/demo/student/rest/StudentController.java
@@ -0,0 +1,46 @@
+package com.example.demo.student.rest;
+
+import com.example.demo.student.Student;
+import com.example.demo.student.StudentRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/student")
+public class StudentController {
+
+    @Autowired
+    private StudentMapper mapper;
+    @Autowired
+    private StudentRepo repo;
+
+    @GetMapping("/")
+    public StudentDto findOne(@RequestParam Long id) {
+        Student entity = repo.findById(id).orElse(null);
+
+        return mapper.toDto(entity);
+    }
+
+    @PostMapping("/")
+    public Student create(@RequestBody Student entity) {
+        return repo.save(entity);
+    }
+
+    @PutMapping("/")
+    public Student update(@RequestBody Student entity) {
+        Student existing = repo.findById(entity.getId()).orElse(null);
+        if (existing == null) {
+            throw new IllegalArgumentException("'Student' entity not found for update!");
+        }
+
+        return repo.save(entity);
+    }
+
+    @DeleteMapping("/")
+    public void delete(@RequestParam Long id) {
+        Student entity = repo.findById(id).orElse(null);
+        if (entity != null) {
+            repo.delete(entity);
+        }
+    }
+}
