5f8ce3748d01b002df6d163a442fb1aa596d724d
Vytautas Kalinauskas <vytautas.kalinauskas@rivile.lt>
2022-11-29 22:00:35 +0200

    one 2 many relations examples

--- spring/spring-boot/src/main/java/com/example/demo/service/MathService.java
+++ spring/spring-boot/src/main/java/com/example/demo/service/MathService.java
@@ -2,6 +2,8 @@ package com.example.demo.service;
 
 import org.springframework.stereotype.Service;
 
+import com.example.demo.entity.University;
+
 @Service
 public class MathService {
 	
@@ -10,6 +12,30 @@ public class MathService {
 		System.out.println("I WAS CALLED");
 	}
 	
+	public Object returnObject() {
+		
+		return new University();
+		
+		
+	}
+	
+	public void useObject() {
+		
+		Object something = returnObject();
+		
+		if(something instanceof String) {
+			
+		}
+		
+		if(something instanceof Integer) {
+			
+		}
+		
+		if(something instanceof University) { 
+			
+		}
+		
+	}
 	
 	
 
